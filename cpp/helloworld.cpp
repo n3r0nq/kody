37d17d2ef6b617842c916c15b1628458e06de159
n3r0nq <nerofsg@gmail.com>
2017-09-20 12:35:53 +0200

    pobieranie danych tekstowych

--- cpp/helloworld.cpp
+++ cpp/helloworld.cpp
@@ -6,9 +6,19 @@
 
 using namespace std;
 
+
 int main(int argc, char **argv)
 {
-	cout<<"Hello world" <<endl;
+    //char imie //deklaracja zmiennej char
+    //char imie[10]; //deklaracja tablicy znakowej
+    string imie; //deklaracja zmiennej string
+    
+	cout<<"Hello world"<<endl; 
+	cout<<"Podaj imie: ";
+    cin >>imie;
+    //cin.getline(imie, 10);
+    
+    cout << "Cześć " <<imie <<endl;
     
 	return 0;
 }
