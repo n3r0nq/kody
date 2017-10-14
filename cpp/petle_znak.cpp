6172f868ee7c7eecdd1a373037ba18f96bf3c079
n3r0nq <nerofsg@gmail.com>
2017-10-14 14:01:25 +0200

    petle_znak -done

--- cpp/petle_znak.cpp
+++ cpp/petle_znak.cpp
@@ -10,12 +10,15 @@ using namespace std;
 int main(int argc, char **argv)
 {
     char zn='0';
-
-    while(zn !='t' || zn !='T' || zn !='n' || zn!='N' )
+    
+    cout<<"Zgaduj!"<<endl<<endl;
+    
+    while(zn !='t' && zn !='T' && zn !='n' && zn !='N' )
     {
         cout<<"Litere:";
         cin>>zn;
     }
+        cout<<"Brawo zgadles!"<<endl;
     
     return 0;
 }
