bb45e3a66d2d1a8824a78911ef0ac5e1779c46b7
n3r0nq <nerofsg@gmail.com>
2017-11-08 12:37:01 +0100

    python,los,typy;lekcja 08-11-2017

--- python/mlotek.py
+++ python/mlotek.py
@@ -3,17 +3,30 @@
 
 from random import randint
 
+def losuj(ileliczb,maksliczb):
+    liczby = []
+    #for i in range(ileliczb):
+    ile = 0  # ilosc wylosowanych liczb
+    while ile<ileliczb:
+        pass
+        liczba = randint(0,maksliczb)
+        if liczby.count(liczba) == 0:
+            liczby.append(liczba)
+            ile += 1
+    print(liczby)
+
 def main(args):
     ileliczb = int(input("Ile liczb chcesz zgadnac?"))
     maksliczb = int(input("Podaj gorny zkres: "))
 
-    liczby = []
-    for i in range(ileliczb):
-        liczba = randint(0,maksliczb)
-        if liczby.count(liczba) == 0:
-            liczby.append(liczba)
+    losuj(ileliczb,maksliczb)
 
-    print(liczby)
+    typy = set()  # pusty zbiór
+    for i in range(ileliczb):
+        typ = input("Podaj typ: ")
+        typy.add(typ)
+
+    print(typy)
 
     return 0
 
---
+++ python/najwieksza_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main(args):
+    a = int(input('Podaj liczbe:'))
+    b = int(input('Podaj liczbe:'))
+    c = int(input('Podaj liczbe:'))
+
+    m=int
+    if m < a
+        m=a
+    if m < b
+        m=b
+    if m < c
+       m=c
+    print(m)
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
