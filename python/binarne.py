421a4f045afff06151b5615087e202969dbf5799
n3r0nq <nerofsg@gmail.com>
2018-01-03 17:33:18 +0100

    binarny/.py; dom

--- python/binarne.py
+++ python/binarne.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from math import floor
+import math
+
 
 def wyszukaj_liniowo(li, el):
     for i in range(0, len(li)):
@@ -10,26 +11,39 @@ def wyszukaj_liniowo(li, el):
     return -1
 
 
-def wyszukaj_bin(li,el):
+def wyszukaj_bin(li, el):
     lewy, prawy = 0, len(li) - 1
     while lewy < prawy:
-        srodek = floor(lewy + prawy) / 2
-        if el <= srodek:
-            return
+        srodek = math.floor((lewy + prawy) / 2)
+        if li[srodek] < el:
+            lewy = srodek + 1
+        else:
+            prawy = srodek
 
-
-
+    if li[lewy] == el:
+        return lewy
+    else:
+        return -1
 
 
 def main(args):
     lista = [4, 3, 7, 0, 2, 3, 1, 9, -4]
-    lista.sort()
-    el = 3
-    print(wyszukaj_liniowo(lista, el))
-    assert wyszukaj_liniowo(lista, 8) == -1
+    lista.sort()  # sortuje tabele od najmniejszej
+    print(lista)
 
-    wyszukaj_bin(lista, el)
+    el = int(input("Jaki element chcesz wyszukac: "))
+    # print(wyszukaj_liniowo(lista, el))
+    # assert wyszukaj_liniowo(lista, 8) == -1
+
     assert wyszukaj_bin(lista, 8) == -1
+    assert wyszukaj_bin(lista, 3) == 4
+    assert wyszukaj_bin(lista, -4) == 0
+    assert wyszukaj_bin(lista, 4) == 6
+
+    if wyszukaj_bin(lista, el) == -1:
+        print("Brak elementu w zbiorze.")
+    else:
+        print("Szukana pozycja dla elementu ", "\'", el,"\'", " to: ", wyszukaj_bin(lista, el))
 
     return 0
 
