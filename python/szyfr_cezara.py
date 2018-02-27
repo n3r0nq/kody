af6e8573f505f18e136da4b1e328c186274e3d56
n3r0nq <nerofgs@gmail.com>
2018-02-28 00:02:45 +0100

    zrobilem co w mojej mocy z cerarem po mojemu, dziala ale dla klucza nie wiekszego od 6...

--- python/szyfr_cezara.py
+++ python/szyfr_cezara.py
@@ -10,48 +10,68 @@ def szyfruj(tekst, klucz):
     #         "ł", "ń", "ó", "ś", "ź", "ż"]
     for i in tekst:
         ascii = ord(i) + klucz
-        if ord(i) == 32:
+        # print(ascii)
+        if ord(i) == 32:  # szyfracja spacji
             ascii = 32
+        if ord(i) == 46:  # szyfracja kropek
+            ascii = 46
         if ascii > 90 and ascii < 97 or ascii > 122:
             ascii -= 26
-        # elif ascii > 122:
-        #     ascii -= 26
+        if i.isdigit() and ascii > 57:  # szyfracja cyfr
+            ascii -= 10
 
         szyfrogram += chr(ascii)
 
     return szyfrogram
 
 
-def deszyfruj(szyfrogram, klucz): # troche duzo ifow, ale narazie nwm jak to przyspieszyc i uproscic. wazne ze dziala
+# troche duzo ifow, ale narazie nwm jak to przyspieszyc i uproscic. wazne ze dziala
+def deszyfruj(szyfrogram, klucz):
     tekst = ""
     for i in szyfrogram:
         ascii = ord(i) - klucz
-        # print(i, ascii)
-        if ord(i) == 32:
+        # print(ascii)
+        if ord(i) == 32:  # deszyfracja spacji
             ascii = 32
-        if ascii < 65 and ascii != 32:
+        if ord(i) == 46:  # deszyfracja kropek
+            ascii = 46
+        if ascii < 65 and ascii != 32 and ascii != 46:
             ascii += 26
-        if ascii > 90 and ascii < 97 and ascii != 32:
+        if ascii > 90 and ascii < 97 and ascii != 32 and ascii != 46:
             ascii += 26
-        if i.isdigit():
-            ascii -= 26
         # print(i, ascii)
+        if i != "." and ascii == 46:  # to niweluje buga sprawiajacego,
+                                     # że 8 => "RS" gdyz linia 37 go powoduje
+            ascii += 26
+        # print(i, ascii)
+
+        if i.isdigit():  # deszyfracja cyfr cyfr
+            # print(ascii)
+            ascii -= 26
+            if ascii < 48:
+                ascii += 10
+            # print(ascii)
         tekst += chr(ascii)
 
     return tekst
 
 # oblsłuzyć male i duże litery
 # obsluzyc spacje
+# sprawdzajka: ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz Bartosz Gnatowski urodzony 02.11.2000 roku w Sandomierzu 0123456789
 
 
 def main(args):
     tekst = input("Podaj tekst: ")
     klucz = int(input("Podaj klucz: "))
+
+    while klucz > 6:
+         klucz = int(input("Podaj klucz: "))
+
     szyfrogram = szyfruj(tekst, klucz)
     # szyfrogram = "VCBIU FHCDUD MHVW SURVWB"
     # klucz = 3
-    print(szyfrogram)
-    print(deszyfruj(szyfrogram, klucz))
+    print("Zaszyfrowane: ", szyfrogram)
+    print("Deszyfrowane: ", deszyfruj(szyfrogram, klucz))
 
     return 0
 
