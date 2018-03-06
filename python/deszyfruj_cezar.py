6da78a0377d7db174e8c7d51d0ce7192f3413b9e
n3r0nq <nerofgs@gmail.com>
2018-03-06 20:21:09 +0100

    cos tam z deszyfracji dodane

---
+++ python/deszyfruj_cezar.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def deszyfruj(szyfrogram, klucz):
+    tekst = ""
+    for i in szyfrogram:
+        ascii = ord(i) - klucz
+        # print(ascii)
+        if ord(i) == 32:  # deszyfracja spacji
+            ascii = 32
+        if ord(i) == 46:  # deszyfracja kropek
+            ascii = 46
+        if ascii < 65 and ascii != 32 and ascii != 46:
+            ascii += 26
+        if ascii > 90 and ascii < 97 and ascii != 32 and ascii != 46:
+            ascii += 26
+        # print(i, ascii)
+        if i != "." and ascii == 46:  # to niweluje buga sprawiajacego,
+                                      # że 8 => "RS" gdyz linia 37 go powoduje
+            ascii += 26
+        # print(i, ascii)
+
+        if i.isdigit():  # deszyfracja cyfr cyfr
+            # print(ascii)
+            ascii -= 26
+            if ascii < 48:
+                ascii += 10
+            # print(ascii)
+        tekst += chr(ascii)
+
+    return tekst
+
+# oblsłuzyć male i duże litery
+# obsluzyc spacje
+# sprawdzajka: ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz Bartosz Gnatowski urodzony 02.11.2000 roku w Sandomierzu 0123456789
+
+
+def main(args):
+    szyfrogram = input("Podaj szyfrogram: ")
+    klucz = int(input("Podaj klucz: "))
+
+    print("Zaszyfrowane: ", szyfrogram)
+    print("Deszyfrowane: ", deszyfruj(szyfrogram, klucz))
+
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
---
+++ python/deszyfruj_vigenere.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def deszyfruj(szyfrogram, haslo):
+    tekst = []
+    # litery = 'AĄBCĆDEĘFGHIJKLŁMNŃOÓPQRSTUVWXYZŹŻ'
+    litery = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    litera_hasla = 0
+
+    for i in szyfrogram:
+        index = litery.find(i)
+        if index != -1:
+            index -= litery.find(haslo[litera_hasla])
+            index %= 26
+            # index %= 34 # dla alfabetu polskiego
+
+            tekst.append(litery[index])
+            litera_hasla += 1
+            if litera_hasla == len(haslo):
+                litera_hasla = 0
+        else:
+            tekst.append(i)
+
+    return "".join(tekst)
+
+
+def main(args):
+    szyfrogram = input("Podaj szyfrogram: ")
+    haslo = input("Podaj haslo: ")
+
+    szyfrogram.upper()
+    haslo.upper()
+
+    print("Zaszyfrowane: ", szyfrogram)
+    print("Deszyfrowane: ", deszyfruj(szyfrogram, haslo))
+
+    return 0
+# =ZNAK(JEŻELI(WIERSZ(A1)+KOLUMNA(A1)+63>90;WIERSZ(A1)+KOLUMNA(A1)+63-26;WIERSZ(A1)+KOLUMNA(A1)+63))
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
