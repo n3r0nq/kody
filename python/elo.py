86cfd53b593cc7b2a44f69f651ed2032e2f999ce
n3r0nq <nerofsg@gmail.com>
2018-09-05 09:39:29 +0200

    elo

---
+++ python/elo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def main(args):
+    imie=input('Jak się nazywasz? ')
+    print("witaj: ", imie)
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
