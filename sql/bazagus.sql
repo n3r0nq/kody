c47b7eb449dcef0dd74e6199a726e5f938a18819
n3r0nq <nerofsg@gmail.com>
2018-10-10 09:39:22 +0200

    sql

---
+++ sql/bazagus.sql
@@ -0,0 +1,39 @@
+-- bazagus.sql
+--CREATE - klauzula
+
+DROP TABLE IF EXISTS miasta;
+DROP TABLE IF EXISTS dane_demograficzne;
+DROP TABLE IF EXISTS powierzchnia;
+
+CREATE TABLE miasta (
+    id_miasta INTEGER PRIMARY KEY AUTOINCREMENT,
+    nazwa_miasta TEXT(30),
+    wojewodztwo TEXT(30)
+);
+
+-- sqlite3 nazwa.db < nazwa.sql
+-- sqlite nazwa.db
+-- sqlite>.table - wyswietla tabele
+-- sqlite>.schema - wyswietla klauzule SQL
+-- sqlute>.quit - wychodzenie z interpretera
+
+CREATE TABLE dane_demograficzne(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    liczba_mieszkancow INTEGER,
+    liczba_kobiet INTEGER,
+    grupa_wiekowa TEXT(15),
+    data_akt DATE,
+    id_miasta INTEGER,
+    FOREIGN KEY (id_miasta) REFERENCES miast(id_miasta)
+
+);
+
+CREATE TABLE powierzchnia(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    powierzchnia_miasta DECIMAL,
+    powierzchnie_zielone DECIMAL,
+    data_akt DATE,
+    id_miasta INTEGER,
+    FOREIGN KEY (id_miasta) REFERENCES miast(id_miasta)
+
+);
