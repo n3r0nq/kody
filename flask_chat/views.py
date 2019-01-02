af38bf80126e596063dabce72eba05abb1aa0877
n3r0nq <nerofsg@gmail.com>
2019-01-02 09:41:18 +0100

    appka

--- flask_chat/views.py
+++ flask_chat/views.py
@@ -4,7 +4,7 @@
 #  app.py
 #
 from flask import Flask
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
 from modele import *
 from forms import *
 
@@ -38,6 +38,17 @@ def quiz():
     return render_template('quiz.html', query=pytania)
 
 
+def flash_errors(form):
+    """Odczytanie wszystkich blad w formularza i przygotowanie komunikatow"""
+    for field, errors in form.errors.items():
+        for error in errors:
+            if type(error) is list:
+                error = error[0]
+            flash("Bład: {}. Pole: {}".format(
+                error,
+                getattr(form, field).label.text))
+
+
 @app.route('/dodaj', methods=['GET', 'POST'])
 def dodaj():
     form = DodajForm()
@@ -52,5 +63,8 @@ def dodaj():
                             pytanie=p.id,
                             odpok=int(o['odpok']))
             odp.save()
+        flash("Dodano pytanie!", "sukces")
         return redirect(url_for('index'))
+    elif request.method == 'POST':
+        flash_errors(form)
     return render_template('dodaj.html', form=form)
