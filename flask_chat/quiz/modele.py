0d4f685a648636b57e95eca11534e46efa86292c
n3r0nq <nerofsg@gmail.com>
2019-01-30 09:40:46 +0100

    lekcja edycja pytan

--- flask_chat/quiz/modele.py
+++ flask_chat/quiz/modele.py
@@ -9,16 +9,21 @@ baza_nazwa = 'quiz.db'
 baza = SqliteDatabase(baza_nazwa)  # instancja bazy
 
 ### MODELE #
+
+
 class BazaModel(Model):
     class Meta:
         database = baza
 
+
 class Kategoria(BazaModel):
     kategoria = CharField(null=False)
+
 
 class Pytanie(BazaModel):
     pytanie = CharField(null=False)
     kategoria = ForeignKeyField(Kategoria, related_name='pytania')
+
 
 class Odpowiedz(BazaModel):
     odpowiedz = CharField(null=False)
--- flask_chat/quiz/views.py
+++ flask_chat/quiz/views.py
@@ -4,7 +4,8 @@
 #  views.py
 #
 from flask import Flask
-from flask import render_template, request, redirect, url_for, flash, abort
+from flask import render_template, request, redirect, url_for, flash
+from flask import abort
 from modele import *
 from forms import *
 
@@ -89,9 +90,40 @@ def usun(pid):
     """Usuwanie pytania o podanym id"""
     p = get_or_404(pid)
     if request.method == 'POST':
-        flash('Usunieto pytanie {}'.format(p.pytanie), 'sukces')
+        flash('Usunięto pytanie {}'.format(p.pytanie), 'sukces')
         for o in Odpowiedz.select().where(Odpowiedz.pytanie == p.id):
             o.delete_instance()
         p.delete_instance()
         return redirect(url_for('index'))
     return render_template('pytanie_usun.html', pytanie=p)
+
+
+@app.route('/edytuj/<int:pid>', methods=['GET', 'POST'])
+def edytuj(pid):
+    """Edycja pytan i odpowiedzi"""
+    p = get_or_404(pid)
+
+    form = DodajForm(obj=p)
+    form.kategoria.choices = [(k.id, k.kategoria) for k in Kategoria.select()]
+    form.kategoria.data = p.kategoria.id
+
+    if form.validate_on_submit():
+        p.pytanie = form.pytanie.data
+        p.kategoria = form.kategoria.data
+        p.save()
+        for o in form.odpowiedzi.data:
+            odp = Odpowiedz.get_by_id(o['id'])
+            odp.odpowiedzi = o['odpowiedz']
+            odp.odpok = int(o['odpok'])
+            odp.save()
+        flash("Zaktualizowano pytanie: {}".format(form.pytanie.data))
+        redirect(url_for('lista'))
+    else:
+        flash_errors(form)
+
+    odpowiedzi = []
+    for o in Odpowiedz.select().where(Odpowiedz.pytanie == p.id).dicts():
+        odpowiedzi.append(o)
+        form.odpowiedzi(data=odpowiedzi)
+
+    return render_template('edytuj.html', form=form)
